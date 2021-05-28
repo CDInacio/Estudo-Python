af13b8096fd2fedcf5b413f6ac28b4fd8a38703f
claudio dantas <claudiodantas16@hotmail.com>
2021-05-28 18:59:41 -0300

    update

--- python poo/aula1/main.py
+++ python poo/aula1/main.py
@@ -3,3 +3,7 @@ from pessoa import Pessoa
 p1 = Pessoa('Cáudio', 24)
 p1.comer('carne')
 p1.comer('carne')
+print(p1.get_ano_nascimento())
+
+# print(Pessoa.ano_atual)
+# print(p1.ano_atual)
--- python poo/aula1/pessoa.py
+++ python poo/aula1/pessoa.py
@@ -1,5 +1,12 @@
+from datetime import datetime
+
+
 class Pessoa:
+    # variável da classe
+    ano_atual = int(datetime.strftime(datetime.now(), '%Y'))
+
     def __init__(self, nome, idade, comendo=False, falando=False):
+        # variaveis de instancia
         self.nome = nome
         self.idade = idade
         self.comendo = comendo
@@ -20,3 +27,26 @@ class Pessoa:
 
         print(f'{self.nome} parou de comer!')
         self.comendo = False
+
+    def falar(self):
+        if self.falando:
+            print(f'{self.nome} já está falando')
+            return
+
+        if self.comendo:
+            print(f'{self.nome}, você não pode falar enquanto come!')
+            return
+
+        print(f'{self.nome} está falando!')
+        self.falando = True
+
+    def para_de_falar(self):
+        if not self.falando:
+            print(f'{self.nome} já parou de falar!')
+            return
+
+        print('Parando de falar!')
+        self.falando = False
+
+    def get_ano_nascimento(self):
+        return self.ano_atual - self.idade
