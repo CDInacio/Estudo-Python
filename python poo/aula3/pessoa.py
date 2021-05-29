ba6bbff87786fdb2902711fc0f5475bd33d841e7
claudio dantas <claudiodantas16@hotmail.com>
2021-05-29 20:17:13 -0300

    update

---
+++ python poo/aula3/pessoa.py
@@ -0,0 +1,33 @@
+# métodos estáticos
+# Um método estático é um método que pode ser chamado sem a instancia da classe.
+
+from random import randint
+
+class Pessoa:
+    # atributo de classe
+    ano_atual = 2021
+
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+
+    def nascimento(self):
+        print(self.ano_atual - self.idade)
+
+    @classmethod
+    def por_ano_nascimento(cls, nome, nascimento):
+        idade = cls.ano_atual - nascimento
+        # retornando a propria classe
+        return cls(nome, idade)
+
+    @staticmethod
+    def gera_id():
+        rand = randint(1, 1000)
+        return rand
+
+
+p1 = Pessoa('claudio', 24)
+# o metodo pode ser chamado tanto pela instancia quanto pela classe
+print(Pessoa.gera_id())
+print(p1.gera_id())
+
---
+++ python poo/aula4/main.py
@@ -0,0 +1,41 @@
+# Getters e Setters
+
+class Produto:
+    def __init__(self, nome, preco):
+        self.nome = nome
+        self.preco = preco
+
+    def desconto(self, percentual):
+        self.preco = self.preco - (self.preco * (percentual / 100))
+
+    # getter
+    @property
+    def nome(self):
+        return self._nome
+
+    # setter
+    @nome.setter
+    def nome(self, valor):
+        self._nome = valor
+
+    # getter
+    @property
+    def preco(self):
+        return self._preco
+
+    # setter
+    @preco.setter
+    def preco(self, valor):
+        # verificando o tipo da variavel
+        if isinstance(valor, str):
+            valor = float(valor.replace('R$', ''))
+        self._preco = valor
+
+
+p1 = Produto('camisa', 100)
+p1.desconto(80)
+print(p1.preco)
+
+p2 = Produto('short', 'R$100')
+p2.desconto(8)
+print(p1.preco)
