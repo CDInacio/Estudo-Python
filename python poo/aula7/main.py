d7d6e9094ead3b807080bb3ebe636705f8c088cb
claudio dantas <claudiodantas16@hotmail.com>
2021-05-30 19:44:04 -0300

    update

---
+++ python poo/aula7/main.py
@@ -0,0 +1,19 @@
+# relação de assosiação
+
+from classes import Escritor
+from classes import Caneta
+from classes import MaquinaDeEscrever
+
+escritor = Escritor('claudio')
+caneta = Caneta('bic')
+maquina = MaquinaDeEscrever()
+
+# associando escritor com caneta
+escritor.ferramenta = maquina
+escritor.ferramenta.escrever()
+
+# essa é uma associação fraca, onde um objeto não depende do outro para existir
+# por exemplo, seu eu apagar o escritor, a caneta vai continuar a existir, como mostrado abaixo
+del escritor
+#print(escritor)
+print(caneta.marca)     # caneta ainda existe
