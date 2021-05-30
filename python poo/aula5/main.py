7f0f424cc1b3d5e8bf7b8bdfe3ab67078bed126f
claudio dantas <claudiodantas16@hotmail.com>
2021-05-30 19:48:42 -0300

    atributos de classe

--- python poo/aula5/main.py
+++ python poo/aula5/main.py
@@ -1,17 +1,17 @@
-class A:
+class X:
     var = 123
 
     def mostrar(self):
         print(self.var - 10)
 
 
-a1 = A()
+a1 = X()
 
 # criando uma variavel de instancia
 a1.var = 123
 # alterando o valor da variavel de classe
-A.var = 321
+X.var = 321
 
 print(a1.var)
-print(A.var)
+print(X.var)
 print(a1.mostrar())
